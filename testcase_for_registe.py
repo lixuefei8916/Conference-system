2b8df2e29184f2cdb5758a9102a7576b1a87f7fd
lixuefei8916 <lixuefei8916@126.com>
2015-12-03 17:26:18 +0800

    5.2：完成MVC修改 + 测试用例 (api --> contorller --> model)
    5.2：完成MVC修改 + 测试用例 (api --> contorller --> model)

--- testcase_for_registe.py
+++ testcase_for_registe.py
@@ -8,7 +8,7 @@ class Test_registe_api(unittest.TestCase):
 	def test_user_Submit(self):
 		self.registe_api = User__()
 		user_Submit = self.registe_api.push_userdetail_to_Controller('example20','202020','example20@lixuefei.com')
-		self.assertEquals(user_Submit,'OK!!!!!!')
+		self.assertEquals(user_Submit,'OK')
 
 if __name__ == '__main__':
 	unittest.main()
